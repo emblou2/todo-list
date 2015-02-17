a4943499beac68ef444dc9029d02109a30a1c27f
Emily Blanchard <emblou2@aol.com>
2015-02-16 19:23:34 -0700

    Skeleton commit

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+/*
+1. Define Angular application (make sure link to Angular library is included)
+2. Inject firebase (make sure link to Firebase library is included)
+3. Create controller
+4. Define routes (put above controller)
+*/
