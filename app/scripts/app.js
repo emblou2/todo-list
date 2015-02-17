1e85803093995f177798a4d709958d0236d282b1
Emily Blanchard <emblou2@aol.com>
2015-02-17 08:08:51 -0700

    added controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,21 @@
 3. Create controller
 4. Define routes (put above controller)
 */
+var app = angular.module("todoApp", ["firebase"]);
+
+app.controller("TodoCtrl", ["$scope", "$firebase",
+  function($scope, $firebase) {
+    var ref = new Firebase("https://popping-torch-9799.firebaseio.com/");
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+    // download the data into a local object
+    $scope.data = sync.$asObject();
+  }
+]);
+
+function TodoCtrl($scope) {
+  $scope.todos =
+  {done: false, text: 'first'},
+  {done: false, text: 'second'}
+  ];
+}
