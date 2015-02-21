3d3645ba225c3256685bd43bc201c37b2861a80c
Alissa <alissalikavec@gmail.com>
2015-02-21 03:30:00 +0000

    Todo fixes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,54 +5,35 @@
 4. Define routes (put above controller)
 5. Use angular's $interval
 */
-var app = angular.module("todoApp", ["firebase", 'ui.router'])
+var app = angular.module("todoApp", ['firebase', 'ui.router'])
 
-    .config(function ($stateProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: 'templates/home.html'
-            })
-            .state('history', {
-                url: '/history',
-                templateUrl: 'templates/history.html',
-                controller: 'TodoCtrl'    
-            })
+.config(function ($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/home.html'
+        })
+        .state('history', {
+            url: '/history',
+            templateUrl: 'templates/history.html'   
+        })
+})
 
-    })
-
-app.controller("TodoCtrl", ["$scope", "$firebase", "$interval",
-    function($scope, $firebase, $interval) { 
-        $scope.callAtInterval = function() { 
-            console.log("Interval works"); 
-        } 
-        $interval( function (){ $scope.callAtInterval }, 3000);
-    });
-        
-
- 
-
-    
-
-        
-
-
+app.controller("TodoCtrl", ["$scope", "$firebase", "$interval", function($scope, $firebase, $interval) { 
+    console.log("I'm the controller!");
     var ref = new Firebase("https://popping-torch-9799.firebaseio.com/");
     // create an AngularFire reference to the data
     var sync = $firebase(ref);
     // download the data into a local object
     $scope.data = sync.$asObject();
 
-
-
     $scope.textTest = "Hello!";
      //var textTest = "Hello!"
 
-
     $scope.todos = [
-    {done: false, text: 'first', archived: true},
-    {done: false, text: 'second', archived: false}
+        {done: false, text: 'first', archived: true},
+        {done: false, text: 'second', archived: false}
     ];
 
     $scope.addTodo = function() {
@@ -64,7 +45,18 @@ app.controller("TodoCtrl", ["$scope", "$firebase", "$interval",
         $scope.todoText = '';
     };
 
-        }
-]);
+    $scope.callAtInterval = function() { 
+        console.log("Interval works"); 
+    };
+
+    $interval( function (){ $scope.callAtInterval(); }, 300, [3]);
+}]);
+
+    
+
+        
+
+
+
 
  
